63bd87eb63f4a73e8658b267f2f6cddb135e3989
Victor Moraes <victormoraespt4@gmail.com>
2025-05-09 02:51:50 -0300

    rebranding

--- react/app/components/Nav.tsx
+++ react/app/components/Nav.tsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
             <path d="m.334 0 4.358 4.359h7.15v7.15l4.358 4.358V0zM.2 9.72l4.487-4.488v6.281h6.28L6.48 16H.2z" />
           </svg>
           <span className="ml-2 text-lg font-semibold text-rose-600 invisible sm:visible">
-            ECommerce
+            EGommerce
           </span>
         </div>
 
